5f39ee4002237121e4c0e26cfacf8a91c257e7bb
Camiloflorez1 <68828746+Camiloflorez1@users.noreply.github.com>
2020-07-27 21:04:11 -0500

    Update Clase 1.R

--- scripts/Camilo Scrips/Clase 1.R
+++ scripts/Camilo Scrips/Clase 1.R
@@ -1,2 +1,16 @@
 # se usa "#" para comentarios que no se ejecutan
 # para ejecutar un código se usa "ALT + Enter"
+# se usa "? + la función a investigar" para la ayuda de los conceptos y ejemplos de R
+# se usa "Help.start()" para abrir los manuales
+#Instalar Paquetes "Install.packages("nombre_paquete", dep = True)
+#Cargar el paquete Library(nombre_paquete)
+
+##Tidy (tidyText) es el paquete para generar marckdown en pdf
+
+#Para separar oraciones u operaciones usar ";"
+
+#suma "+" Resta "-" Multiplicación "*" División "/" Potencia "^" Cociente entero "%/%" Resto de división entera "%%"
+# se unsa "." para decimales y "," para separar
+
+
+
